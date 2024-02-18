863ebafe63b920d12f21d687b2827ee18dbb035c
startupmasterau <159253254+startupmasterau@users.noreply.github.com>
2024-02-18 22:16:56 +1100

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,41 @@
     👉 Event Listeners
     👉 Higher order Function (Math.random())
 */
-
+// variable to keep track of score
 let totalScore = {computerScore: 0, playerScore: 0}
+var globalPlayername = {} // empty global object to store playername to use in other functions
 
+// event Listener for when enter key is pressed in the inputName field
+const playerInput = document.getElementById("nameInput")
+playerInput.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    console.log("Enter worked")
+    event.preventDefault();
+    document.getElementById("nameButton").click();
+  }
+})
+
+// Get name from #nameInput and on enter display it after user hits "Go" and then hide the input field
+function getNameValue() {
+  const playerInput = document.getElementById("nameInput")
+  const playerName = playerInput.value
+  globalPlayername.name = playerName //store this in the globalPlayername object
+  const playerNameButton = document.getElementById("nameButton")
+  const playerNameDiv = document.querySelector(".nameInput")
+  const gameSectionDiv = document.querySelector(".gameSection")
+
+  playerInput.style.display = "none"
+  playerNameButton.style.display = "none"
+  gameSectionDiv.style.visibility = "visible"
+
+  // Display playername if entered
+  if (playerName.trim().length != 0) {
+    playerNameDiv.innerHTML = "<span class='animate-charcter'>"+playerName+", Let's play!</span>"
+  }
+  else {
+    playerNameDiv.innerHTML = "<span class='animate-charcter'>Human, Let's play!</span>"
+  }
+}
 // ** getComputerChoice randomly selects between `Rock` `Paper` `Scissors` and returns that string **
 // getComputerChoice() 👉 'Rock'
 // getComputerChoice() 👉 'Scissors'
@@ -21,7 +53,7 @@ function getComputerChoice() {
   let computerChoice = rpsChoices[Math.floor(Math.random() * 3)]
   return computerChoice
 }
-test = getComputerChoice()
+//test = getComputerChoice()
 // console.log(test)
 
 // ** getResult compares playerChoice & computerChoice and returns the score accordingly **
@@ -52,35 +84,58 @@ function getResult(playerChoice, computerChoice) {
   return score
 }
 
-
 // test2 = getResult("Rock", test)
 // console.log({test2})
 // console.log(`computer is ${test}`)
 
+// Create computerChoice button live div innerHTML to display
+function createComputerButton(computerChoice) {
+  switch(computerChoice) {
+    case "Rock":
+      buttonHTML = "<div id='buttonCompChoice'>✊</div>";
+        break;
+    case "Scissors":
+      buttonHTML = "<div id='buttonCompChoice'>✌</div>";
+        break;
+    case "Paper":
+      buttonHTML = "<div id='buttonCompChoice'>🤚</div>";
+        break;
+  }
+    return buttonHTML
+} 
 // ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
-function showResult(score, playerChoice, computerChoice) {
+function showResult(score, buttonHTML) {
   // Hint: on a score of -1
   // You should do result.innerText = 'You Lose!'
   // Don't forget to grab the div with the 'result' id!
-
   const resultDiv = document.getElementById("result")
   const playerScoreDiv = document.getElementById("player-score")
   const handsDiv = document.getElementById("hands")
 
   if (score == 1) {
-    resultDiv.innerText = "You Win!"
+    resultDiv.innerHTML = "<h2 class='resultH2' id='resultWin' style='color:green;'>You Win!</h2>"
   }
   else if (score == 0) {
-    resultDiv.innerText = "It's a Draw!"
+    resultDiv.innerHTML = "<h2 class='resultH2' id='resultDraw' style='color:white;'>It's a Draw!</h2>"
   }
   else if (score == -1) {
-    resultDiv.innerText = "You Lose!"
+    resultDiv.innerHTML = "<h2 class='resultH2' id='resultWin' style='color:red;'>You Lose!</h2>"
   }
   else {
     resultDiv.innerText = "Oops!! Something went wrong"
   }
+  // get globalPlayer object name property if present
+  // console.log(globalPlayername.name)
+  // if (globalPlayername.name != "") {    
+  // handsDiv.innerText = `👨🏻‍🦰${globalPlayername.name}:${playerChoice} vs 🤖${computerChoice}` //get emoji with Windows key + .
+  // } 
+  // else {
+  // handsDiv.innerText = `👨🏻‍🦰Human:${playerChoice} vs 🤖${computerChoice}`
+  // }
+
+  // use the buttonHTML from createComputerButton() to display the button of the computerChoice
+  handsDiv.innerHTML = buttonHTML
   
-  handsDiv.innerText = `👨🏻‍🦰${playerChoice} vs 🤖${computerChoice}` //get emoji with Windows key + .
   playerScoreDiv.innerText = `👨🏻‍🦰: ${totalScore.playerScore} 🤖: ${totalScore.computerScore}`
 }
 
@@ -90,15 +145,9 @@ function onClickRPS(playerChoice) {
   const score = getResult(playerChoice, computerChoice)
   totalScore.playerScore += score // alternatively you can access the property using totalScore['playerScore']
   totalScore.computerScore -= score
-  
-  // console.log({computerChoice})
-  // console.log({playerChoice})
-  // console.log({score})
-  // console.log(score[1])
-  // console.log(totalScore)
-  showResult(score, playerChoice, computerChoice)
+  createComputerButton(computerChoice) // this function passes computerChoice from here to determine what innerHTML to show for Computer's move button like div
+  showResult(score, buttonHTML)
 }
-
 
 // ** Make the RPS buttons actively listen for a click and do something once a click is detected **
 function playGame() {
@@ -115,13 +164,12 @@ function playGame() {
     rpsButton.onclick = () => onClickRPS(rpsButton.value)
   })
 
-
   // Add a click listener to the end game button that runs the endGame() function on click
   const endGameButton = document.getElementById('endGameButton')
   endGameButton.onclick = () => endGame(totalScore)
 }
 
-// ** endGame function clears all the text on the DOM **
+// ** endGame function clears all the text on the DOM and resets the score **
 function endGame(totalScore) {
   totalScore.playerScore = 0
   totalScore.computerScore = 0
